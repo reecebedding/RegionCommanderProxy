4da9c1a563fa23151e007ca630be9ca6f331195a
Reece Bedding <reecebedding@hotmail.co.uk>
2015-03-19 14:27:17 +0000

    Added Blacklist helper util with search function

--- utils/RCBlackListHelper.js
+++ utils/RCBlackListHelper.js
@@ -1,7 +1,32 @@
 (function(RCBlack){
     
-        RCBlack.Search = function(searchTerm, next){
-            return "Yes";
-        };
+    var util = require("util");
+    var http = require("http");
+    var request = require("request");
+    
+    var botKey = "THIS NEEDS TO BE SET";
+   
+    
+    RCBlack.Search = function(searchTerm, next){
+        
+        var options = {
+            host: "http://rc.goonfleet.com",
+            port: 80,
+            path: util.format("/rcbot/cbl/%s/%s", botKey, searchTerm)
+        }
+        
+        var requestString = options.host + options.path
+        
+        request(requestString, function(error, response, body){
+           if(!error && response.statusCode == 200){
+               console.log("Response aquired from RC");
+               var responseContent = JSON.parse(body);
+               next(null, responseContent);
+           }else {
+               console.log("Response failed from RC. Error: " + error);
+               next(error, null);
+           }
+        });        
+    };
     
 })(module.exports);
